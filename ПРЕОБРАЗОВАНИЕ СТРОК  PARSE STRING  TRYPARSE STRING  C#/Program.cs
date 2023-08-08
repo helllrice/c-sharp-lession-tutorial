995ddc5f64ad61ac115662ed17c880585445257a
Vasiliy <63386773+helllrice@users.noreply.github.com>
2023-08-08 22:52:07 +0300

    parse string

---
+++ ПРЕОБРАЗОВАНИЕ СТРОК  PARSE STRING  TRYPARSE STRING  C#/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+//конвртация строки
+//    parse и tryparse
+
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // усложненный метод парсинга tryparse
+
+            string str = "1";
+
+            int a;
+
+            bool result = int.TryParse(str, out a);
+
+            Console.WriteLine(result);
+
+            if (result)
+            {
+                Console.WriteLine("операция успешна, значение = " + a);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось конвертировать!");
+            }
+
+            // простой метод парсинга 
+
+            string str2 = "2";
+            int a2 = int.Parse(str2);
+
+            Console.WriteLine(a2);
+
+            
+        }
+
+
+    }
+
+}
