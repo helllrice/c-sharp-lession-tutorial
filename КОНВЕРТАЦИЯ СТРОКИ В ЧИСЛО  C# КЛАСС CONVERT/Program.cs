2690852d7a64588fcc00558cd04c2a445fd73a09
Vasiliy <63386773+helllrice@users.noreply.github.com>
2023-08-08 22:12:53 +0300

    учу с#
    тут я изучаю синтаксис

---
+++ КОНВЕРТАЦИЯ СТРОКИ В ЧИСЛО  C# КЛАСС CONVERT/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+//конвртация строки
+//    класс Convert
+
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b;
+
+            Console.WriteLine("Введите число 1");
+            
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+            Console.WriteLine("Введите число 2");
+
+            str = Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+
+            Console.WriteLine("Сумма чисел = " + result);
+
+
+            string str2 = "1.9";
+
+            //Предоставляет сведения для конкретного языка и региональных параметров для числовых значений форматирования и анализа.
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            double a2 = Convert.ToDouble(str2, numberFormatInfo);
+        }
+
+
+    }
+
+
+}
