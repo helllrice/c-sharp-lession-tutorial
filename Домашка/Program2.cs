4345cee0d9f97e4dd8559d9b872ca6b503bbb421
Vasiliy <63386773+helllrice@users.noreply.github.com>
2023-09-02 17:22:08 +0300

    домашка по проверки числа на четность

---
+++ Домашка/Program2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnC_
+// домашка 
+// проверка числа на четность 
+
+
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string str;
+            Console.WriteLine("Введите число");
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число не четное");
+            }
+        }
+
+    }
+
+}
+
