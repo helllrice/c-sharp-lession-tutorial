91e45aa3dc1c720ea8ba452706c4d2d7f5c037d1
Vasiliy <63386773+helllrice@users.noreply.github.com>
2023-09-02 19:36:26 +0300

    калькулятор дядя

---
+++ Домашка/Program3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnC_
+{
+    internal class Program
+    {
+
+        // мега простой калькулятор в консоли
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            string action;
+
+            Console.WriteLine("Введите первое число");
+            firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите первое число");
+            secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(firstNumber + secondNumber);
+                    break;
+                case "-":
+                    Console.WriteLine(firstNumber - secondNumber);
+                    break;
+                case "*":
+                    Console.WriteLine(firstNumber * secondNumber);
+                    break;
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("на ноль делить нельзя!!!!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(firstNumber / secondNumber);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Ошибка!!!!! такой операции нету в списке");
+                    break;
+            }
+        }
+    }
+}
